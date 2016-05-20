49c390ae5b50e06a2a0dea9f49fa71d524f4e52b
谢晓阳 <28midu@gmail.com>
2016-05-20 10:35:29 +0800

    add tee command to start scripts

--- startclient.sh
+++ startclient.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ./test_UDPRawClient 192.168.1.105 192.168.1.100 2> log |
+tee without-encode.h264 |
 cvlc --demux=h264 - &
 
 ./test_UDPEncodedClient 192.168.1.105 192.168.1.100  2> log |
+tee with-encode.h264 |
 cvlc --demux=h264 - &
--- startencodedclient.sh
+++ startencodedclient.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./test_UDPEncodedClient 192.168.1.105 192.168.1.100  2> log |
+tee with-encode.h264 |
 vlc --demux=h264 -
